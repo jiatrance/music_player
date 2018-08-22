7759b4132e678e179aedf3eb0a5f13636f176c1d
jiatrance <40256459+jiatrance@users.noreply.github.com>
2018-08-21 18:43:23 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/theme.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music Player',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: Color(0xffdddddd),
+          onPressed: () {},
+        ),
+        title: Text(''),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            color: Color(0xffdddddd),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Container(
+                width : 125.0,
+                height : 125.0,
+                child: Image.network('http://img0.imgtn.bdimg.com/it/u=453406035,498615673&fm=214&gp=0.jpg'),
+              ),
+            )
+          ),
+
+          Container(
+            width: double.infinity,
+            height: 125.0,
+          ),
+          Container(
+            width: double.infinity,
+            color: accentColor,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40.0, bottom: 50.0),
+              child: Column(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                        text: 'Song Title\n',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 4.0,
+                          height: 1.5,
+                        )),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: 'Artist Name',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.75),
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 3.0,
+                          height: 1.0,
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: Container(),),
+                        IconButton(icon: Icon(
+                          Icons.skip_previous,
+                          color: Colors.white,
+                          size: 35.0,
+                          ),
+                          onPressed: (){},
+                        ),
+                        Expanded(child: Container(),),
+                        RawMaterialButton(
+                          shape: CircleBorder(),
+                          fillColor: Colors.white,
+                          splashColor: lightColor,
+                          highlightColor: lightColor.withOpacity(0.5),
+                          elevation: 6.0,
+                          highlightElevation: 3.0,
+                          onPressed: (){},
+                          child: Padding(
+                            padding:EdgeInsets.all(8.0),
+                            child:Icon(
+                              Icons.play_arrow,
+                              color:darkAccentColor,
+                              size:35.0
+                            )
+                          ),
+                        ),
+                        Expanded(child: Container(),),
+                        IconButton(icon: Icon(
+                          Icons.skip_next,
+                          color: Colors.white,
+                          size: 35.0,
+                          ),
+                          onPressed: (){},
+                        ),
+                        Expanded(child: Container(),),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color accentColor=const Color(0xfff08f8f);
+const Color lightColor=const Color(0xffffafaf);
+const Color darkAccentColor=const Color(0xffd06f6f);
